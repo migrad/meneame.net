4c56087c52f87e35f82a0d9c1fcbc54c46988f9e
Migrad <migrad@gmail.com>
2024-11-23 00:51:11 +0100

    Add method for posts search.
    Took 1 hour 19 minutes

--- www/search/SearchMySQL.php
+++ www/search/SearchMySQL.php
@@ -16,6 +16,9 @@ class SearchMySQL implements iSearch
         if ($_REQUEST['w'] === 'comments') {
             return $this->db_get_search_comments($by_date, $start, $count);
         }
+        else if ($_REQUEST['w'] === 'posts') {
+            return $this->db_get_search_posts($by_date, $start, $count);
+        }
         else {
             return $this->db_get_search_links($by_date, $start, $count);
         }
@@ -23,7 +26,7 @@ class SearchMySQL implements iSearch
 
     private function db_get_search_links($by_date = false, $start = 0, $count = 50)
     {
-        global $db, $globals;
+        global $db;
         // For now it serves for search specific blogs (used from most voted sites)
 
         $response = array();
@@ -52,7 +55,7 @@ class SearchMySQL implements iSearch
                 break;
             default:
                 if ($_REQUEST['w'] === 'links') $this->where = "link_content_type = 'text' and ";
-                if ($_REQUEST['w'] === 'posts') $this->where = "link_content_type = 'article' and ";
+                if ($_REQUEST['w'] === 'articles') $this->where = "link_content_type = 'article' and ";
                 $this->where .= "match(link_tags,link_content,link_title,link_url) against ('$url' IN BOOLEAN MODE)";
         }
 
@@ -92,7 +95,7 @@ class SearchMySQL implements iSearch
 
     private function db_get_search_comments($by_date = false, $start = 0, $count = 50)
     {
-        global $db, $globals;
+        global $db;
         // For now it serves for search specific blogs (used from most voted sites)
 
         $response = array();
@@ -104,7 +107,7 @@ class SearchMySQL implements iSearch
 
         if ($_REQUEST['h']) {
             $hours = (int)$_REQUEST['h'];
-            $this->where .= " and link_date > date_sub(now(), interval $hours hour)";
+            $this->where .= " and comment_date > date_sub(now(), interval $hours hour)";
         }
 
         $this->order = "comment_date desc";
@@ -115,6 +118,46 @@ class SearchMySQL implements iSearch
 
         if ($this->where && $this->from) {
             $sql = "select comment_id $this->fields from $this->from where $this->where order by $this->order limit $start,$count";
+            $response['rows'] = $db->get_var("select count(*) from $this->from where $this->where");
+            if ($response['rows'] > 0) {
+                $response['ids'] = array();
+
+                foreach ($db->get_col($sql) as $id) {
+                    $response['ids'][] = $id;
+                }
+            }
+        }
+
+        $response['time'] = microtime(true) - $start_time;
+
+        return $response;
+    }
+
+    private function db_get_search_posts($by_date = false, $start = 0, $count = 50)
+    {
+        global $db;
+        // For now it serves for search specific blogs (used from most voted sites)
+
+        $response = array();
+        $start_time = microtime(true);
+        $url = $db->escape($_REQUEST['q']);
+
+        $this->from = 'posts';
+        $this->where = "match(post_content) against ('$url' IN BOOLEAN MODE)";
+
+        if ($_REQUEST['h']) {
+            $hours = (int)$_REQUEST['h'];
+            $this->where .= " and post_date > date_sub(now(), interval $hours hour)";
+        }
+
+        $this->order = "post_date desc";
+        if ($_REQUEST['o'] === 'relevance') {
+            $this->fields = ',' . $this->where . ' as score ';
+            $this->order = 'score desc';
+        }
+
+        if ($this->where && $this->from) {
+            $sql = "select post_id $this->fields from $this->from where $this->where order by $this->order limit $start,$count";
             $response['rows'] = $db->get_var("select count(*) from $this->from where $this->where");
             if ($response['rows'] > 0) {
                 $response['ids'] = array();
